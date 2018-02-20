3476040fa985d86440cdb03a94826a277f4f689e
Nate Gay <nate.gay@wpengine.com>
2018-02-20 09:33:24 -0600

    binary tree lookup working

--- dictionary/mydict.py
+++ dictionary/mydict.py
@@ -1,52 +1,88 @@
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.left = None
+        self.right = None
+
 class MyDict:
-    keys = []
-    values = []
+    node = None
 
     def __init__(self):
         """construct a dictionary"""
         pass
 
-    def put(self, key, value):
+    def keytoindex(self, key):
+        return int.from_bytes(
+            bytearray(key, encoding='utf8'),
+            byteorder='big'
+        )
+
+    def indextokey(self, index):
+        return index.to_bytes(
+            (index.bit_length() + 7) // 8,
+            byteorder='big', signed=True
+        ).decode("utf-8")
+
+    def put(self, key, value, node=None):
         """
         insert an item with key->value into the dictionary
         time complexity: 1
         """
-        self.keys.append(key)
-        self.values.append(value)
+        intkey = self.keytoindex(key)
 
-    def get(self, key):
+        if self.node is None:
+            self.node = Node(intkey, value)
+        elif node is None:
+            self.put(key, value, self.node)
+        elif node.key > intkey:
+            if node.left is None:
+                node.left = Node(intkey, value)
+            else:
+                self.put(key, value, node.left)
+        else:
+            if node.right is None:
+                node.right = Node(intkey, value)
+            else:
+                self.put(key, value, node.right)
+
+    def get(self, key, node=None):
         """
         get an item with the given key from the dictionary
-        time complexity: n, ugh
+        time complexity: log(n)
         """
-        index = self.keys.index(key)
-        return self.values[index]
+        intkey = self.keytoindex(key)
+
+        if self.node is None:
+            return None
+
+        if node is None:
+            return self.get(key, self.node)
+        elif node.key > intkey:
+            return self.get(key, node.left)
+        elif node.key < intkey:
+            return self.get(key, node.right)
+
+        return self.indextokey(node.key), node.value
 
     def delete(self, key):
         """
         delete an item with the given key from the dictionary
         time complexity: 1
         """
-        index = self.keys.index(key)
-        del self.keys[index]
-        del self.values[index]
-        return None
 
-    def show(self):
-        return list(zip(self.keys, self.values))
+    def show(self, node):
+        if not node:
+            return
+        self.show(node.left)
+        print(self.indextokey(node.key), node.value)
+        self.show(node.right)
 
 
 if __name__ == '__main__':
-    meow = MyDict()
-    meow.put("poke", 12345)
-    meow.put(1, "blah")
-    meow.put("quack", "duck")
-    print(meow.show())
-    print(meow.get("poke"))
-    print(meow.delete("quack"))
-    print(meow.show())
-    #print(meow.get("ayay"))
-
-
-# int key to bytes
-# key.to_bytes((key.bit_length() + 7) // 8, byteorder='big', signed=True)
+    mydict = MyDict()
+    mydict.put("duck", "quack")
+    mydict.put("bear", "roar")
+    mydict.put("dog", "woof")
+    mydict.show(mydict.node)
+    print(mydict.get("bear"))
