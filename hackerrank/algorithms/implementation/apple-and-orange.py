2427bb1f28b85338f221243ffa238fc9c5658dee
Nate Gay <nate.gay@wpengine.com>
2018-04-01 23:35:19 -0500

    reducing duplication

--- hackerrank/algorithms/implementation/apple-and-orange.py
+++ hackerrank/algorithms/implementation/apple-and-orange.py
@@ -5,15 +5,13 @@ def countApplesAndOranges(s, t, a, b, apples, oranges):
     house_min, house_max = s, t
     apple_tree, orange_tree = a, b
 
-    apples_hit_tree = 0
-    for apple in apples:
-        if house_min <= apple + apple_tree <= house_max:
-            apples_hit_tree += 1
+    print(impactingFruit(house_min, house_max, apple_tree, apples))
+    print(impactingFruit(house_min, house_max, orange_tree, oranges))
 
-    oranges_hit_tree = 0
-    for orange in oranges:
-        if house_min <= orange + orange_tree <= house_max:
-            oranges_hit_tree += 1
 
-    print(apples_hit_tree)
-    print(oranges_hit_tree)
+def impactingFruit(house_min, house_max, fruit_tree, fruits_dropped):
+    fruit_hit_house = 0
+    for fruit in fruits_dropped:
+        if house_min <= fruit + fruit_tree <= house_max:
+            fruit_hit_house += 1
+    return fruit_hit_house
