e2e366d05f6597e8a2099f60d60696d87bb80249
Nate Gay <nate.gay@wpengine.com>
2018-04-07 23:36:16 -0500

    Remove adjacent duplicates

---
+++ sluyter/remove-adjacent-duplicates.py
@@ -0,0 +1,25 @@
+"""
+[2:07 PM] Michael Sluyter: remove duplicates -- given a string, remove *adjacent* duplicates. so for example
+[2:08 PM] Michael Sluyter:
+    "1aaaaaa2aaaaaa3bbb4444q"
+[2:08 PM] Michael Sluyter: output would be:
+[2:08 PM] Michael Sluyter:
+    1a2a3b4q
+"""
+
+import unittest
+
+
+def remove_adjacent_duplicates(string):
+    return "".join([string[i] for i in range(0, len(string)) if i - 1 < 0 or string[i - 1] != string[i]])
+
+
+class TestMe(unittest.TestCase):
+    def test_1(self):
+        string = "1aaaaaa2aaaaaa3bbb4444q"
+        expect = "1a2a3b4q"
+        self.assertEqual(remove_adjacent_duplicates(string), expect)
+
+
+if __name__ == '__main__':
+    unittest.main()
